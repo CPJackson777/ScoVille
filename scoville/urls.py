00b07740b749c4f6ab8cbdcde395ab2466db99ce
Charles Jackson <cpjackson572@gmail.com>
2020-03-16 10:57:44 -0500

    added paths in urls.py files

--- scoville/urls.py
+++ scoville/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from scovilleapp.models import * #This is needed so that Django knows about the location of your models.
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('scovilleapp.urls')),
 ]
