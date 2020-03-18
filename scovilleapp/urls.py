52905e350b173e9ecc4d9a5b9dd4b357098c53da
Charles Jackson <cpjackson572@gmail.com>
2020-03-18 13:31:05 -0500

    adding signup view

--- scovilleapp/urls.py
+++ scovilleapp/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.urls import include, path
 from .views import *
+# from sc import views as core_views
 
 app_name = "scovilleapp"
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
 ]
