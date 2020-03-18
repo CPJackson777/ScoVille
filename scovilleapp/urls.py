6cf7c817976f0f1cb2fbeb880d3be894330894d0
Charles Jackson <cpjackson572@gmail.com>
2020-03-18 14:58:32 -0500

    register 2nd attempt

--- scovilleapp/urls.py
+++ scovilleapp/urls.py
@@ -1,12 +1,13 @@
-from django.conf.urls import url
-from django.urls import include, path
+# from django.conf.urls import url, include
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from .views import *
 
 
 app_name = "scovilleapp"
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('logout/', logout_user, name='logout'),
-    path('^signup/$', signup, name='signup'),
+    path(r'accounts/', include('django.contrib.auth.urls')),
+    path(r'^logout/$', logout_user, name='logout'),
+    path(r'^register/$', register_user, name='register'),
 ]
