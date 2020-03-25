e419ea41bc59d8f4f61211a3924e1c5c03481642
Charles Jackson <cpjackson572@gmail.com>
2020-03-25 12:27:35 -0500

    still working on getting blog template to display correctly

--- scovilleapp/views/blog_posts/blog_post.py
+++ scovilleapp/views/blog_posts/blog_post.py
@@ -3,12 +3,14 @@ from django.shortcuts import redirect, render, reverse
 from scovilleapp.models import BlogPost
 from ..connection import Connection
 from scovilleapp.models import model_factory
+from django.contrib.auth.decorators import login_required
+
 
 def blogpost_list(request):
     if request.method == 'GET':
-        all_blogposts = BlogPost.objects.all()
+        all_blogposts = BlogPost.objects.all().order_by('created_on')
 
-        template = 'blog_post/blogpost_list.html'
+        template = 'blog_posts/blogpost_list.html'
         context = {
             'all_blogposts': all_blogposts
         }
