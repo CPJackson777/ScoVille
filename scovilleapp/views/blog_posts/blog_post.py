6334a9088aa4077dcc02ce973671fb42d4b4a04e
Charles Jackson <cpjackson572@gmail.com>
2020-03-25 11:21:14 -0500

    fixed blogpost view to make browswer show the right thing

--- scovilleapp/views/blog_posts/blog_post.py
+++ scovilleapp/views/blog_posts/blog_post.py
@@ -1,12 +1,12 @@
 import sqlite3
 from django.shortcuts import redirect, render, reverse
-from scovilleapp.models import ScovilleScale
+from scovilleapp.models import BlogPost
 from ..connection import Connection
 from scovilleapp.models import model_factory
 
 def blogpost_list(request):
     if request.method == 'GET':
-        all_blogposts = ScovilleScale.objects.all()
+        all_blogposts = BlogPost.objects.all()
 
         template = 'blog_post/blogpost_list.html'
         context = {
