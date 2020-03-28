a60bfaa65e32c2d408f3f3a961d8201a283da5f5
Charles Jackson <cpjackson572@gmail.com>
2020-03-28 02:21:50 -0500

    got logged in user to be redirected to scoville scale page

--- scovilleapp/views/auth/register.py
+++ scovilleapp/views/auth/register.py
@@ -36,7 +36,7 @@ def register_user(request):
         # If authentication was successful, log the user in
         if authenticated_user is not None:
             login(request=request, user=authenticated_user)
-            return redirect(reverse('libraryapp:books'))
+            return redirect(reverse('scovilleapp:home'))
 
         else:
             # Bad login details were provided. So we can't log the user in.
